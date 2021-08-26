964fde4e36884dc54ef624a4faeb0317675e2552
pereymavyu <pereyma.ic@gmail.com>
2021-08-27 01:30:31 +0700

    arraylist initial commit

---
+++ MyArrayList/src/ru/academits/pereyma/myarraylist/MyArrayList.java
@@ -0,0 +1,151 @@
+package ru.academits.pereyma.myarraylist;
+
+import java.util.*;
+import java.util.function.UnaryOperator;
+
+public class MyArrayList<E> implements List<E> {
+    private E[] items;
+    private int length;
+
+    @Override
+    public int size() {
+        return length;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return length == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        for (E e : items) {
+            if (e.equals(o)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] result = new Object[length];
+
+        if (length >= 0) {
+            System.arraycopy(items, 0, result, 0, length);
+        }
+
+        return result;
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return new T[5];
+    }
+
+    @Override
+    public boolean add(E e) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void replaceAll(UnaryOperator<E> operator) {
+
+    }
+
+    @Override
+    public void sort(Comparator<? super E> c) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, E element) {
+
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    @Override
+    public Spliterator<E> spliterator() {
+        return null;
+    }
+}
